6fb81157221aadc27582c40566e881abb6cb7229
Zitronus <lmn.mdz1@gmail.com>
2024-11-11 01:50:08 +0800

    Unang instance ni felimon

---
+++ Contact.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Contact us</h1>
+    <div class="collapse" id="primarymenu-collapse">
+        		<div class="navmenu">
+					<ul>
+						<li><a href="http://localhost:3000/index.php">Home</a></li>
+						<li><a href="http://localhost:3000/About.php">About Us</a></li>
+						<li><a href="http://localhost:3000/Contact.php">Contact Us</a></li>
+											</ul>
+        		</div>
+      	</div>
+    	</div>
+</body>
+</html>

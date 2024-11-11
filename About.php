d5205ce5ca77dc2f2218b70a2dabef0ac9823588
givano750 <givano550@gmail.com>
2024-11-11 14:09:36 +0800

    trial commit 2

--- About.php
+++ About.php
@@ -17,5 +17,34 @@
         		</div>
       	</div>
     	</div>
+		<div id="page-inner">
+	<div class="container">
+		<h2 class="pagetitle">Contact Us</h2>
+			  	<div class="row">
+	      <div class="col-md-4">
+	        	<div class="sidebar">
+	         	<p><img alt="" src="https://buenamano.ph/getfile/contact-us-6524f87f1487f.jpg" /></p>
+	        	</div>
+	      </div>
+	      <div class="col-md-8">
+	        	<div class="content-area">
+	        		<p>Need assistance? Property inquiries? Customer feedback? We&rsquo;d love to hear from you!<br />
+We are always happy to help. Get in touch with us today!</p>
+
+<p>Customer Care Hotline: +63 (2) 85803158<br />
+Customer Care Mobile: +63 (927) 1336086<br />
+Customer Care Fax: +63 (2) 85803126<br />
+Email: &nbsp;<a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="7f3d2a3a313e323e31303f1d0f16511c1012510f17">[email&#160;protected]</a><br />
+Website: www.buenamano.ph<br />
+Learn more about our properties, ask questions or submit an offer, drop by our Main Office at:</p>
+
+<p>Strategic Asset Management and Sales (SAMS)<br />
+Ground Floor, BPI Buendia Center<br />
+360 Senator Gil Puyat Avenue<br />
+Makati City, Philippines 1229</p>
+
+<p>Our official business days/hours are Mondays to Fridays from 8:30am &ndash; 5:30pm<span style="color:#2980b9"> <strong>(best time to call is around 2:00pm - 3:00pm).</strong></span><br />
+You may also visit any of our SAMS&rsquo; offices in the provincial Retail Loan Centers.</p>
+
 </body>
 </html>

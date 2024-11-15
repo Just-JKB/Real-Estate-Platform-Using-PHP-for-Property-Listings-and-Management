fa10798a9e9a0ee719c156ac6837379977ba2b5f
givano750 <givano550@gmail.com>
2024-11-15 22:37:18 +0800

    about (ud)

--- About.php
+++ About.php
@@ -3,35 +3,41 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Clavem</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>About us</h1>
-    <div class="collapse" id="primarymenu-collapse">
-        		<div class="navmenu">
-					<ul>
-						<li><a href="http://localhost:3000/index.php">Home</a></li>
-						<li><a href="http://localhost:3000/About.php">About Us</a></li>
-						<li><a href="http://localhost:3000/Contact.php">Contact Us</a></li>
-											</ul>
-        		</div>
+<nav>
+<div class="logo">About</div>
+
+<ul class="navbar">
+		    <li><a href="index.php">Home</a></li>
+		    <li><a href="About.php">About Us</a></li>
+		    <li><a href="Contact.php">Contact Us</a></li>
+	    </ul>
+    </nav>
+	</div>
       	</div>
     	</div>
+		<br>
+		<br>
+		<br>
+		<br>
 		<div id="page-inner">
 	<div class="container">
-		<h2 class="pagetitle">About Us</h2>
 			  	<div class="row">
 	      <div class="col-md-4">
 	        	<div class="sidebar">
 	        	</div>
 	      </div>
-		  <p> CLAVEM is a wide range of property choices located in Lipa City area, for all types of buyers and investor <br />
+		  <p> <b>CLAVEM</b> is a wide range of property choices located in Lipa City area, for all types of buyers and investor <br />
 		<br />
 		Our selection of foreclosed properties includes House and Lots, Apartments, Townhouses, Condominium Units, Office Spaces, Buildings, Vacant Lots, Warehouses, as well as Agricultural Lands, Commercial Spaces, and Industrial Sites.
 		  </p>
 		  <p> Green Tags, Yellow Tags, and Red Tags are the official branding/classifications for our Buena Mano properties for sale (our properties are color-coded for your easy reference): <br />
 
-GREEN TAG: Real estate properties that assure buyers a value-for money purchase and an efficient and worry-free post-sale documentation process.<br />
+<br><b>GREEN TAG</b>: Real estate properties that assure buyers a value-for money purchase and an efficient and worry-free post-sale documentation process.<br />
 <br />
 <br />
 Titles / Tax Declarations registered under the name of the Bank.<br />
@@ -40,7 +46,7 @@ With For Sale signages<br />
 With authorized caretakers<br />
 <br />
 <br />
-YELLOW TAG: We offer real estate properties with "Special Concerns" which our Buena Mano Sales Team readily discloses to interested buyers. Examples may include, but are not limited to; <br />
+<b>YELLOW TAG</b>: We offer real estate properties with "Special Concerns" which our Buena Mano Sales Team readily discloses to interested buyers. Examples may include, but are not limited to; <br />
 <br />
 <br />
 Titles / Tax Declarations in process of consolidation<br />
@@ -48,7 +54,7 @@ Titles with technical description errors, Reconstitution (Sec. 7 RA 26), Sec. 4 
 No possession of property (with former owner, tenants, or illegal settlers)<br />
 <br />
 <br />
-RED TAG: Our experienced Sales Team can assist you and provide win-win solutions for our Buena Mano Red Tag properties with legal concerns. We value your needs and concerns, thus our commitment to transparency and full disclosure of all material information.<br />
+<b>RED TAG</b>: Our experienced Sales Team can assist you and provide win-win solutions for our Buena Mano Red Tag properties with legal concerns. We value your needs and concerns, thus our commitment to transparency and full disclosure of all material information.<br />
 <br />
 <br />
 Pending court cases against the Bank filed by the previous owner or 3rd party claimants.<br />

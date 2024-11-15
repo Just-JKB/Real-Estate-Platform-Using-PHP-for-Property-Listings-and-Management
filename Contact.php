5bda7e3da554a43933fbe3ec82f17e5f29451b24
givano750 <givano550@gmail.com>
2024-11-15 22:27:00 +0800

    contact ud

--- Contact.php
+++ Contact.php
@@ -1,47 +1,41 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Clavem</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Contact us</h1>
-    <div class="collapse" id="primarymenu-collapse">
-        		<div class="navmenu">
-					<ul>
-						<li><a href="http://localhost:3000/index.php">Home</a></li>
-						<li><a href="http://localhost:3000/About.php">About Us</a></li>
-						<li><a href="http://localhost:3000/Contact.php">Contact Us</a></li>
-											</ul>
-        		</div>
-      	</div>
-    	</div>
-		<div id="page-inner">
-	<div class="container">
-		<h2 class="pagetitle">Contact Info</h2>
-			  	<div class="row">
-	      <div class="col-md-4">
-	        	<div class="sidebar">
-	        	</div>
-	      </div>
-	      <div class="col-md-8">
+<nav>
+<div class="logo">CLAVEM</div>
+<ul class="navbar">
+		    <li><a href="index.php">Home</a></li>
+		    <li><a href="About.php">About Us</a></li>
+		    <li><a href="Contact.php">Contact Us</a></li>
+	    </ul>
+    </nav>
+	<br>
+	<br>
+	<br>
+	<br>
+	<div class="col-md-8">
 	        	<div class="content-area">
-				<p>Need assistance? Property inquiries? Customer feedback? We wouldd love to hear from you!<br />
+				<p>Need assistance? Property inquiries? Customer feedback? We would love to hear from you!<br />
 We are always happy to help. Get in touch with us today!</p>
 
-<p>Customer Care Hotline: +63 122345678<br />
-Customer Care Mobile: +63 9556789094<br />
-Customer Care Fax: +63 9945005100<br />
-Email: <a>23-33529@g.batstate-u.edu.ph</a><br/>
-Website: www.clavem.ph<br />
-Learn more about our properties, ask questions or submit an offer, drop by our Main Office at:</p>
-
-<p>BSIT 2102<br />
+<p>Customer Care Hotline: <b>+(123) 123-4567</b><br />
+Customer Care Mobile: <b>+63 9556789094</b><br />
+<br><b>Email(s): </b>	<br> <b>Filemon - </b><a href="23-36130@g.batstate-u.edu.ph">23-36130@g.batstate-u.edu.ph</a><br/>
+        <b>Gian - </b><a href="23-33529@g.batstate-u.edu.ph">23-33529@g.batstate-u.edu.ph</a><br/>
+		<b>Justine - </b><a href="23-33704@g.batstate-u.edu.ph">23-33704@g.batstate-u.edu.ph</a><br/>
+<br><b>Website: </b><a href="www.clavem.ph">www.clavem.ph</a><br />
+<br>
+Learn more about our properties, ask questions or submit an offer at:</p>
+<b><p>BSIT-2102<br />
 Batangas State University<br />
-Marauoy<br />
-Lipa City, Philippines</p>
-
+Marauoy, Lipa City, Batangas<br />
+Philippines</p></b>
 </body>
 </html>

f7960eb2de50a64f94c67ac93794010f52dba9e3
Filemon <lmn.mdz1@gmail.com>
2024-11-15 21:37:26 -0700

    version 2

--- About.php
+++ About.php
@@ -3,19 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Clavem</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>About us</h1>
-    <div class="collapse" id="primarymenu-collapse">
-        		<div class="navmenu">
-					<ul>
-						<li><a href="http://localhost:3000/index.php">Home</a></li>
-						<li><a href="http://localhost:3000/About.php">About Us</a></li>
-						<li><a href="http://localhost:3000/Contact.php">Contact Us</a></li>
-											</ul>
-        		</div>
-      	</div>
-    	</div>
+<nav>
+<div class="logo">About</div>
+
+<ul class="navbar">
+		    <li><a href="index.php">Home</a></li>
+		    <li><a href="About.php">About Us</a></li>
+		    <li><a href="Contact.php">Contact Us</a></li>
+	    </ul>
+   
+<div class="search">
+    <form>
+        <input type="text" class="input" placeholder="Search...">
+        <button class="sbutton" type="submit">Search</button>
+    </form>
+    </nav>
 </body>
 </html>
--- index.php
+++ index.php
@@ -53,11 +53,29 @@ $property_classes = [
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Clavem</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+
+<nav>
+<div class="logo">Clavem</div>
+
+<ul class="navbar">
+		    <li><a href="index.php">Home</a></li>
+		    <li><a href="About.php">About Us</a></li>
+		    <li><a href="Contact.php">Contact Us</a></li>
+	    </ul>
+   
+<div class="search">
+    <form method="POST" action="search.php">
+        <input type="text" class="input" placeholder="Search...">
+        <button class="sbutton" type="submit">Search</button>
+    </form>
+    </nav>
+
 <div class="container mt-5">
     <h3>Find Properties for Sale</h3>
-    <form method="POST" action="search_results.php">
+    <form method="POST" action="search.php">
         <!-- Category  -->
         <div class="form-group">
             <label for="category">Category</label>
